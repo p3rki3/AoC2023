973549247bbfead30734ca4cfdab980cb1c79dbc
Simon <41059048+p3rki3@users.noreply.github.com>
2023-12-06 10:04:34 +0000

    Day 6 (optimised) and run times tracker

--- Day6_solution.py
+++ Day6_solution.py
@@ -1,17 +1,8 @@
 import AoCFramework as AoC
-
-def part_1(puzzle):
-    answer = 1
-    for (t, d) in puzzle:
-        raceswon = 0
-        for i in range(t):
-            if(i * (t-i) > d):
-                raceswon += 1
-        answer *= raceswon
-    return answer
+from math import prod
 
 AoC.Init(None)
 Puzzle1 = [(41, 244), (66, 1047), (72, 1228), (66, 1040)]
 Puzzle2 = [(41667266, 244104712281040)]
 AoC.verify(74698, 27563421)
-AoC.run(part_1(Puzzle1), part_1(Puzzle2))
+AoC.run(prod((t - 2*int(t/2 - (t*t/4 - d)**.5) - 1) for (t, d) in Puzzle1), sum((t - 2*int(t/2 - (t*t/4 - d)**.5) - 1) for (t, d) in Puzzle2))
