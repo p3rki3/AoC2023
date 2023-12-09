f5a33073d81a596f0f7f0a1eef5475341da67138
Simon <41059048+p3rki3@users.noreply.github.com>
2023-12-09 07:49:26 +0000

    Day 9 Solution and updated runtimes

---
+++ Day9_solution.py
@@ -0,0 +1,13 @@
+import AoCFramework as AoC
+
+def get_next(numline):
+    diffs, lastnos = numline, [numline[-1]]
+    while sum(abs(df) for df in diffs) > 0:
+        diffs = [diffs[i+1] - diffs[i] for i in range(len(diffs) - 1)]
+        lastnos.append(diffs[-1])
+    return sum(lastnos)
+
+AoC.verify(1584748274, 1026)
+Lines = AoC.Init("data/day9.txt")[0]
+Nums = [[int(n) for n in line.split()] for line in Lines]
+AoC.run(sum(get_next(numline) for numline in Nums), sum(get_next(list(reversed(numline))) for numline in Nums))
