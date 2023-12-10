a21805586ef5c75c96ccd17934a1bb0188ac476b
Simon <41059048+p3rki3@users.noreply.github.com>
2023-12-10 10:14:33 +0000

    Revised Day 10 Solution

--- Day10_solution.py
+++ Day10_solution.py
@@ -1,76 +1,51 @@
 import AoCFramework as AoC
-from collections import deque
 
-def myneighbours(i, j, maxi, maxj):
-    return [(i, j) for i, j in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)] if 0 <= i < maxi and 0 <= j < maxj]
+def pipe(c, i, j):
+    match c:
+        case '|': return (i-1,j), (i+1,j)
+        case '-': return (i,j-1), (i,j+1)
+        case 'L': return (i-1,j), (i,j+1)
+        case 'J': return (i-1,j), (i,j-1)
+        case '7': return (i+1,j), (i,j-1)
+        case 'F': return (i+1,j), (i,j+1)
 
-def fltr(board, points):
-    return points if Part == 2 else [(i,j) for i,j in points if i < len(board) and j < len(board[0]) and i >= 0 and j >= 0]
-
-def isnextto(board, i, j, a, b):
-    if board[a][b] == '.':
-        return []
-    elif board[a][b] == '|':
-        return fltr(board, [(i-1, j), (i+1, j)])
-    elif board[a][b] == '-':
-        return fltr(board, [(i, j-1), (i, j+1)])
-    elif board[a][b] == 'L':
-        return fltr(board, [(i-1, j), (i, j+1)])
-    elif board[a][b] == 'J':
-        return fltr(board, [(i, j-1), (i-1, j)])
-    elif board[a][b] == '7':
-        return fltr(board, [(i+1, j), (i, j-1)])
-    elif board[a][b] == 'F':
-        return fltr(board, [(i, j+1), (i+1, j)])
-    elif board[a][b] == 'S':
-        return [(ii, jj) for ii, jj in myneighbours(i, j, len(board), len(board[0])) if (i, j) in isnextto(board, ii, jj, ii, jj)]
-    print("Oooooopsie - don't understand this board; should never have gotten here; stopping now!")
-    exit(1)
+def connected_to(pt):
+    return set(pipe(Pipes[*pt], *pt))
 
 def part_1():
-    global Found, Startcoord
-    for r, line in enumerate(Lines):
-        for c, ch in enumerate(line):
-            if ch == 'S':
-                Startcoord = (r,c)
-    (i, j) = Startcoord
-    found, queue = set([(i, j)]), deque([(i, j)])
-    while len(queue) > 0:
-        i, j = queue.popleft()
-        for neighbours in isnextto(Lines, i, j, i, j):
-            if neighbours not in found and Lines[neighbours[0]][neighbours[1]] != '.':
-                queue.append(neighbours)
-                found.add(neighbours)
-    Found = found
-    return len(found) // 2
+    global Visited
+    endpt1, endpt2 = [pt for di,dj in ((-1,0),(0,1),(0,-1),(1,0)) if (pt:=(Start[0]+di,Start[1]+dj)) in Pipes and Start in connected_to(pt)]
+    for c in '|-LJ7F':
+        if set(pipe(c, *Start)) == set([endpt1, endpt2]):
+            Pipes[Start] = c
+    Visited, steps = set([Start, endpt1, endpt2]), 1
+    while endpt1 != endpt2:
+        endpt1, endpt2 = (connected_to(endpt1) - Visited).pop(), (connected_to(endpt2) - Visited).pop()
+        Visited |= set([endpt1, endpt2])
+        steps += 1
+    return steps
 
 def part_2():
-    global Part
-    (i,j), Part, maxi, maxj = Startcoord, 2, len(Lines) * 2, len(Lines[0]) * 2
-    points, points2 = [(2 * i, 2 * j) for i, j in Found], []
-    for ii, jj in points:
-        iii, jjj = ii // 2, jj // 2
-        for neighbour in isnextto(Lines, ii, jj, iii, jjj):
-            points2.append(neighbour)
-    checkpoints = set(points + points2)
-    starts = set([(2 * i + 1, 2 * j + 1), (2 * i - 1, 2 * j + 1), (2 * i + 1, 2 * j - 1), (2 * i - 1, 2 * j - 1)])
-    for start in starts:
-        visited, queue, dontanswer = set([start]), deque([start]), False
-        while(len(queue) > 0):
-            i, j = queue.popleft()
-            for neighbour in myneighbours(i, j, maxi, maxj):
-                if neighbour in checkpoints:
-                    continue
-                if neighbour == (0,0):
-                    dontanswer = True
-                    break
-                if neighbour not in visited:
-                    queue.append(neighbour)
-                    visited.add(neighbour)
-        if not dontanswer:
-            return sum(1 for i, j in visited if i % 2 == 0 and j % 2 == 0)
-    return("Couldn't find a solution!")
+    enclosed = 0
+    for i in range(len(Lines)):
+        inside = False
+        for j in range(len(Lines[0])):
+            if (i,j) in Visited:
+                if Pipes[i,j] in '|LJ':
+                    inside = not inside
+                continue
+            if inside: enclosed += 1
+    return enclosed
+   
+def process_file():
+    global Pipes, Start
+    Pipes = dict()
+    for i, line in enumerate(Lines):
+        for j, ch in enumerate(line):
+            if ch == 'S': Start = (i,j)
+            elif ch != '.': Pipes[i,j] = ch
 
-Part, Found, Startcoord, Lines = 1, None, None, AoC.Init("data/day10.txt")[0]
+Pipes, Start, Visited, Lines = None, None, None, AoC.Init("data/day10.txt")[0]
+process_file()
 AoC.verify(6856, 501)
 AoC.run(part_1, part_2)
