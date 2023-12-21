145014970d8b22039dfd9c5e6afec6d5b52f3f5b
Simon <41059048+p3rki3@users.noreply.github.com>
2023-12-21 09:12:43 +0000

    Day 21 Solution revised

--- Day21_solution.py
+++ Day21_solution.py
@@ -2,7 +2,7 @@ import AoCFramework as AoC
 from collections import deque
 
 def solve():
-    Factors, nextfactor, p1answer, p2answer, goalsteps, Pos, dirs = [0, 0, 0], 0, 0, 0, 26501365, {Startpos}, [(1, 0),(0, 1), (-1, 0) ,(0, -1)]
+    Factors, nextfactor, p1answer, p2answer, goalsteps, Pos, dirs, target = [0, 0, 0], 0, 0, 0, 26501365, {Startpos}, [(1, 0),(0, 1), (-1, 0) ,(0, -1)], Rows // 2
     for count in range(1, 1000):
         npos = set()
         for r, c in Pos:
@@ -11,15 +11,15 @@ def solve():
                 if Grid[nr % Rows][nc % Cols] != "#":
                     npos.add((nr, nc))
         Pos = npos
-        if count == 64:
-            p1answer = len(Pos)
-        elif count % Rows == goalsteps % Rows:
+        if count == target + Rows * nextfactor:
             Factors[nextfactor] = len(Pos)
             nextfactor += 1
             if nextfactor == 3:
                 delta0, delta1, delta2 = Factors[0], Factors[1] - Factors[0], Factors[2] - 2 * Factors[1] + Factors[0]
                 p2answer = delta0 + delta1 * (goalsteps // Rows) + delta2 * ((goalsteps // Rows) * ((goalsteps // Rows) - 1) // 2)
                 return p1answer, p2answer
+        elif count == 64:
+            p1answer = len(Pos)
 
 Lines = AoC.Init("data/day21.txt", test=False)[0]
 Grid = [[char for char in line] for line in Lines]
